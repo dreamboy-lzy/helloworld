3291ad07549c8e499f5ae8c5d38bdf1f08be6239
Ziyun Li <ziyun.li@shopee.com>
2021-01-06 15:56:45 +0800

    代码提交

---
+++ src/main/java/com/lzy/web/HelloWordController.java
@@ -0,0 +1,13 @@
+package com.lzy.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+       return "Hello World! Spring Boot";
+    }
+}
